a8b52e13d415609b18727e9aaa32365886eeefb8
Olha Zhelieva <dorarashkova@gmail.com>
2023-06-11 21:10:20 +0300

    corrected

--- script.js
+++ script.js
@@ -4,26 +4,26 @@
 
 // Задача на повернення ініціалів для кожного імені з масиву, посортованих в алфавітному порядку:
 
-const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let initials;
+// const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+// let initials;
 
-initials = userNames.map((userName) => 
-    userName.split(" ").map((w) => w.substring(0,1).toUpperCase() + '.' ).join(' ')
-).sort();
+// initials = userNames.map((userName) => 
+//     userName.split(" ").map((w) => w.substring(0,1).toUpperCase() + '.' ).join(' ')
+// ).sort();
 
-console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+// console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
 
 // Задача на розворот числа:
 
-const currentMaxValue = 4589;
-let reverseMaxValue;
+// const currentMaxValue = 4589;
+// let reverseMaxValue;
 
-reverseMaxValue = currentMaxValue.toString().split("").reverse().join('');
-reverseMaxValue = Number(reverseMaxValue);
+// reverseMaxValue = currentMaxValue.toString().split("").reverse().join('');
+// reverseMaxValue = Number(reverseMaxValue);
 
-console.log(reverseMaxValue); // 9854
-console.log(typeof reverseMaxValue); // 'number'
+// console.log(reverseMaxValue); // 9854
+// console.log(typeof reverseMaxValue); // 'number'
 
 
 
@@ -32,10 +32,8 @@ console.log(typeof reverseMaxValue); // 'number'
 const resultsArray = [1, 2, [3, [4]]];
 let productOfArray;
 
-productOfArray = 1;
-
-for (let i = 0; i < resultsArray.flat(2).length; i++) {
-    productOfArray = productOfArray * resultsArray.flat(2) [i];
-}
+productOfArray = resultsArray.flat(Infinity).reduce(function(a, b) {
+    return a * b;
+});
 
 console.log(productOfArray); // 24
