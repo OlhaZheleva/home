64ab8a16ee70861fb3ef49c48ee0e58da82b31de
OlhaZheleva <dorarashkova@gmail.com>
2023-07-10 23:07:16 +0300

    rewrite 3

--- turn-on.js
+++ turn-on.js
@@ -1,44 +1,51 @@
 'use strict';
 
 const btnTurn = document.querySelector('.button');
-const theme = document.querySelector('.wrapper'); 
-const lastChanchText = document.querySelector('.last-turn');
-let lastChanchTime = localStorage.getItem('lastChanch');
+const wrapperBackground = document.querySelector('.wrapper'); 
+const lastChangeText = document.querySelector('.last-turn');
+let lastChangeTime = localStorage.getItem('lastChange');
+let theme = localStorage.getItem('theme');
+
 
 
 // запуск сторінки (перше відкритта або перезавантаження)
-if (localStorage.getItem('theme') === 'dark') { // якщо в нас темна тема, а дефолтна світла, то значить і є змінна коли було перемикання теми 
-    theme.classList.add('dark');
-    btnTurn.innerHTML = 'Turn on'; 
-    lastChanchText.innerHTML = `<p>Last turn off: <br> ${lastChanchTime}</p>`;
+
+if (theme === 'dark') { // якщо в нас темна тема, а дефолтна світла, то значить і є змінна коли було перемикання теми 
+    wrapperBackground.classList.add('dark');
+    btnTurn.textContent = 'Turn on'; 
+    lastChangeText.innerHTML = `<p>Last turn off: <br> ${lastChangeTime}</p>`;
     btnTurn.classList.add('purple');
 };
-
-if (localStorage.getItem('lastChanch') !== null) {
-    lastChanchText.innerHTML = `<p>Last turn on: <br> ${lastChanchTime} </p>`; // якщо світла тема, то перевірити чи було перемикання до цього 
+if (theme !== null) {
+    lastChangeText.innerHTML = `<p>Last turn on: <br> ${lastChangeTime} </p>`; // якщо світла тема, то перевірити чи було перемикання до цього 
 } else { 
-    lastChanchText.innerHTML = `<p>No action yet</p>`; // при першому відкритті, коли локал чистий
+    lastChangeText.innerHTML = `<p>No action yet</p>`; // при першому відкритті, коли локал чистий
 };
 
 
 btnTurn.addEventListener('click' , function () { 
 
-    theme.classList.toggle('dark');
-    btnTurn.classList.toggle('purple');
+    if ( wrapperBackground.classList.contains('dark')) { // якщо тема вже темна, увімкнути світлу тему + змінити кнопку
+        wrapperBackground.classList.remove('dark');
+        btnTurn.classList.remove('purple'); 
+        btnTurn.textContent = 'Turn off';
+        localStorage.setItem('theme', 'light');
 
-    if ( theme.classList.contains('dark')) { // якщо тема вже темна, увімкнути світлу тему + змінити кнопку
-        btnTurn.innerHTML = 'Turn on';
-        window.localStorage.setItem('theme', 'light');
-
-    } else { // якзо тема світла, то увімкнути темну  + зніми стилі кнопки 
-        btnTurn.innerHTML = 'Turn off';
-        window.localStorage.setItem('theme', 'dark');
+    } else { // якщо тема світла, то увімкнути темну  + зніми стилі кнопки 
+        wrapperBackground.classList.add('dark');
+        btnTurn.classList.add('purple'); 
+        btnTurn.textContent = 'Turn on';
+        localStorage.setItem('theme', 'dark');
     }
 
-    let lastChanchTime = new Date().toLocaleString(); // записати час дії, тобто кліку по кнопці у локал 
-    lastChanchText.innerHTML = `<p>Last turn off: <br> ${lastChanchTime}</p>`;
-    window.localStorage.setItem('lastChanch', lastChanchTime); 
+    let lastChangeTime = new Date().toLocaleString(); // записати час дії, тобто кліку по кнопці у локал 
+    lastChangeText.innerHTML = `<p>Last turn off: <br> ${lastChangeTime}</p>`;
+    localStorage.setItem('lastChange', lastChangeTime); 
+
 });
 
 
 // але мені здається, що тут можна ще переписати до більш локанічної форми 
+
+// через toggle чогось воно некоректно працює, бо, як я розумію, в нас обовʼязково має бути умова, типу кнопка не просто перемикає, 
+// а виходячи з початкової теми або додає клас, або видаляє 
