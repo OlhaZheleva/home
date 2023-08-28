f6be6c39c497bca836266e76214b4c985ef01c6b
OlhaZheleva <dorarashkova@gmail.com>
2023-08-28 19:14:06 +0300

    added if

--- iterators.js
+++ iterators.js
@@ -3,74 +3,81 @@
 // Задача 1 - ‘FizzBuzz’ через ітератор та генератор
 
 // через ітератор
+
 // let range = {
-//     from: 1,
-//     to: 100,
+//   from: 1,
+//   to: 100,
+// };
 
-//     [Symbol.iterator]() {
-//       this.current = this.from;
-//       return this;
-//     },
+// range[Symbol.iterator] = function () {
+//   let current = this.from;
+//   let last = this.to;
 
+//   return {
 //     next() {
-//       if (this.current <= this.to) {
-//         return { done: false, value: this.current++ };
-//       } else {
+//       if (current > last) {
 //         return { done: true };
+//       } else if (current % 15 === 0) {
+//         current++; 
+//         return { done: false, value: 'FizzBuzz' };
+//       } else if ( current % 3 === 0) {
+//         current++; 
+//         return { done: false, value: 'Fizz' };
+//       } else if (current % 5 === 0) {
+//         current++; 
+//         return { done: false, value:'Buzz' };
+//       } else  {
+//         return { done: false, value: current++ };
 //       }
-//     }
+//     },
 //   };
+// };
 
-//   for (let num of range) {
-//     if (num % 15 === 0)  {
-//         console.log("FizzBuzz");
-//     } else if (num % 3 === 0) {
-//         console.log("Fizz");
-//     } else if (num % 5 === 0) {
-//         console.log("Buzz");
-//     } else {
-//         console.log(num);
-//     }
-//   };
+// for (let num of range) {
+//   console.log(num);
+// }
 
 // через генератор
 
-// function* limitationGenerator(start, end) {
-//   for (let i = start; i <= end; i++) {
-//     yield i;
-//   }
-// }
+// через генератор
 
-// const numberCount = limitationGenerator(1, 100);
+function* limitationGenerator(start, end) {
+  for (let i = start; i <= end; i++) {
+    if (i % 15 === 0) {
+      yield "FizzBuzz";
+    } else if (i % 3 === 0) {
+      yield "Fizz";
+    } else if (i % 5 === 0) {
+      yield "Buzz";
+    } else {
+      yield i;
+    }
+  }
+}
 
-// for (let value of numberCount) {
-//   if (value % 15 === 0) {
-//     console.log("FizzBuzz");
-//   } else if (value % 3 === 0) {
-//     console.log("Fizz");
-//   } else if (value % 5 === 0) {
-//     console.log("Buzz");
-//   } else {
-//     console.log(value);
-//   }
-// }
+function useGenerator(min, max) {
+  const numberCount = limitationGenerator(min, max);
+
+  for (let i = min; i <= max; i++) {
+    console.log(numberCount.next().value);
+  }
+}
+
+useGenerator(1, 100);
 
 // Задача 2 - генератор випадкових чисел
 // Напишіть функцію-генератор, яка генерує випадкові числа.
 // Функція приймає параметрами максимальне значення рандомного числа та
 // кількість рандомних чисел які функція повертає:
 
+// function* generateRandomNumbers(max, quantity) {
 
-function* generateRandomNumbers(max, quantity) {
+//   for (let i = 0; i < quantity; i++) {
+//     yield Math.floor(Math.random() * max);
+//   }
 
-  for (let i = 0; i < quantity; i++) {
-    yield Math.floor(Math.random() * max);
-  }
+// }
 
-}
-
-for (const num of generateRandomNumbers(8, 17)) {
-  console.log(num);
-}
-
-
+// for (const num of generateRandomNumbers(8, 17)) {
+//   console.log(num);
+// }
